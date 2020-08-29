d5c7e8415d77093afddb9328bc37da70c0e0bc9c
Tarek Elkheir <elkheirt@wit.edu>
2020-08-29 09:55:43 -0400

    Created a button to generate new maze

--- main.js
+++ main.js
@@ -1,36 +1,63 @@
 // Tarek Elkheir
-let width = 500;
-let height = 500;
-let size = 50;
+let width = 400;
+let height = 400;
+let size = 100;
 let gridCells = [] // holds all cells in the grid
 let cellsVisited = [] // stack that keeps track of the cells visited in the DFS
 let numCellsVisited = 0
+let numRows = Math.floor(width / size)
+let numCol = Math.floor(height / size)
+let bgColor = '#f2f2f2'
+let newMazeBtn = document.getElementById('new-maze-btn')
+let playerPos = [0, 0]
+let sol = []
+
+newMazeBtn.onclick = function() {
+    init()
+}
 
 function setup() {
     createCanvas(width, height)
-    background('#f0f0f0')
-    var numRows = Math.floor(width / size)
-    var numCol = Math.floor(height / size)
+    init()
+}
+
+function init() {
+    background(bgColor)
     for(var i = 0; i < numRows; i++) {
         gridCells[i] = []
         for(var j  = 0; j < numCol; j++) {
-            var cell = new Cell(i, j, size, [])
+            var cell = new Cell(i, j, size, [], '#0077ff')
             gridCells[i].push(cell)
         }
     }
     cellsVisited.push(gridCells[0][0])
+    drawMaze(gridCells[0][0])
     numCellsVisited++
 }
 
+function keyPressed() {
+    switch(keyCode) {
+        case LEFT_ARROW:
+            
+        case RIGHT_ARROW:
+
+        case UP_ARROW:
+            
+        case DOWN_ARROW:
+            
+    }
+}
+
 function draw() {
-    var numRows = Math.floor(width / size)
-    var numCol = Math.floor(height / size)
     for(var i = 0; i < numRows; i++) {
         for(var j  = 0; j < numCol; j++) {
             gridCells[i][j].show()
         }
     }
-    if(!(cellsVisited.length === 0)) {
+}
+
+function drawMaze(currentCell) {
+    while(!(cellsVisited.length === 0)) {
         var currentCell = cellsVisited.pop()
         currentCell.visited = true
         var neighbors = getNeighbors(currentCell)
@@ -38,7 +65,6 @@ function draw() {
             cellsVisited.push(currentCell)
             var index = Math.floor(Math.random() * neighbors.length)
             cellsVisited.push(neighbors[index])
-            neighbors[index].highlight()
             removeWalls(currentCell, neighbors[index])
             numCellsVisited++
         }
@@ -46,28 +72,30 @@ function draw() {
 }
 
 function removeWalls(cellA, cellB) {
-    if(Math.floor((cellA.x - cellB.x) / size) === 1) {
+    var xDiff = Math.floor((cellA.x - cellB.x) / size)
+    var yDiff = Math.floor((cellA.y - cellB.y) / size)
+    if(xDiff === 1) {
         cellA.walls[2] = false
         cellB.walls[3] = false
     }
-    if(Math.floor((cellA.x - cellB.x) / size) === -1) {
+    if(xDiff === -1) {
         cellA.walls[3] = false
         cellB.walls[2] = false
     }
-    if(Math.floor((cellA.y - cellB.y) / size) === 1) {
+    if(yDiff === 1) {
         cellA.walls[0] = false
         cellB.walls[1] = false
     }
-    if(Math.floor((cellA.y - cellB.y) / size) === -1) {
+    if(yDiff === -1) {
         cellA.walls[1] = false
         cellB.walls[0] = false
     }
 }
 
 function getNeighbors(cell) {
-    var neigbors = []
+    var neighbors = []
     var x = Math.floor(cell.x / size)
-    var y = Math.floor(cell.y / size)
+    var y = Math.floor(cell.y / size)   
     var maxRow = Math.floor((height - 1) / size)
     var maxCol = Math.floor((width - 1) / size)
     var indices = [
@@ -80,29 +108,30 @@ function getNeighbors(cell) {
         if(index[0] >= 0 && index[1] >= 0 && index[0] <= maxRow && index[1] <= maxCol) {
             var neighboringCell = gridCells[index[0]][index[1]]
             if(!neighboringCell.visited) {
-                neigbors.push(neighboringCell)
+                neighbors.push(neighboringCell)
             }
         }
     })
-    return neigbors
+    return neighbors
 }
 
-function Cell(x, y, size, walls) {
+function Cell(x, y, size, playerColor) {
     this.x = x*size
     this.y = y*size
     this.size = size
     this.walls = [true, true, true, true]
     this.visited = false;
+    this.playerColor = playerColor
+    this.cellColor = bgColor
 
     this.highlight = function() {
         noStroke()
-        fill('#5c84ff');
+        fill('#0066ff');
         rect(this.x, this.y, this.size, this.size)
     }
 
     this.show = function() {
         stroke(0)
-        strokeWeight(5)
         strokeCap(SQUARE);
         if(this.walls[0]) {
             line(this.x, this.y, (this.x + this.size), this.y) // top
@@ -118,7 +147,7 @@ function Cell(x, y, size, walls) {
         }
         if(this.visited) {
             noStroke()
-            fill('#f0f0f0');
+            fill(bgColor);
             rect(this.x, this.y, this.size, this.size)
         }
     }
